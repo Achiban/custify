4a5c9092068299eb14183eba8d69020d9da94eac
Achiban <oussamaachiban6@gmail.com>
2026-04-11 17:21:11 +0100

    setup env

---
+++ src/main/java/com/custify/CustifyApplication.java
@@ -0,0 +1,13 @@
+package com.custify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustifyApplication.class, args);
+	}
+
+}
